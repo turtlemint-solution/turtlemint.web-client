7d223b27316fbc75698f4bcadeff92ac2421b5df
turtlemint-solution <turtlemint.solution@gmail.com>
2022-12-10 20:04:01 +0530

    segregated data on the basis of provider

--- turtlemint/src/dashboard-module/components/insurances/car/car-table.tsx
+++ turtlemint/src/dashboard-module/components/insurances/car/car-table.tsx
@@ -10,8 +10,18 @@ import {
   Th,
   Thead,
   Tr,
+  Text,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
 } from "@chakra-ui/react";
-import { CarInsuranceDekho } from "../../../../common/mock-data/CarData";
+import {
+  CarHdfcErgo,
+  CarInsuranceDekho,
+  CarPolicyBazar,
+} from "../../../../common/mock-data/CarData";
 
 interface CarTableProps {
   maker: string;
@@ -24,29 +34,505 @@ interface CarTableProps {
 const CarTable: FC<{}> = (): JSX.Element => {
   return (
     <div>
-      <TableContainer>
-        <Table variant="simple">
-          <TableCaption>Insurance Dekho</TableCaption>
-          <Thead>
-            <Tr>
-              <Th>Company</Th>
-              <Th>Premium</Th>
-              <Th>Policy Type</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {CarInsuranceDekho?.map((el: any, index: number) => {
-              return (
-                <Tr>
-                  <Td>{el.provider}</Td>
-                  <Td>{el.premiumAmount}</Td>
-                  <Td>{el.policy}</Td>
-                </Tr>
-              );
-            })}
-          </Tbody>
-        </Table>
-      </TableContainer>
+      <Tabs>
+        <TabList>
+          <Tab>Bajaj</Tab>
+          <Tab>digit</Tab>
+          <Tab>United India Insurance</Tab>
+          <Tab>Sriram</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <TableContainer>
+              <Text
+                ml={2}
+                fontWeight="bold"
+                textTransform="uppercase"
+                fontSize="lg"
+                letterSpacing="wide"
+                color="teal.600"
+              >
+                Insurance Dekho
+              </Text>
+              <Table variant="simple">
+                <Thead>
+                  <Tr>
+                    <Th>Company</Th>
+                    <Th>Premium</Th>
+                    <Th>Policy Type</Th>
+                    <Th>IDV</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {CarInsuranceDekho?.map((el: any, index: number) => {
+                    return (
+                      <>
+                        {el.provider === "bajaj" ? (
+                          <Tr>
+                            <Td>{el.provider}</Td>
+                            <Td>{el.premiumAmount}</Td>
+                            <Td>{el.policy}</Td>
+                            <Td>{el.idv}</Td>
+                          </Tr>
+                        ) : (
+                          <></>
+                        )}
+                      </>
+                    );
+                  })}
+                </Tbody>
+              </Table>
+            </TableContainer>
+            <TableContainer>
+              <Text
+                ml={2}
+                fontWeight="bold"
+                textTransform="uppercase"
+                fontSize="lg"
+                letterSpacing="wide"
+                color="teal.600"
+              >
+                Policy Bazar
+              </Text>
+              <Table variant="simple">
+                <Thead>
+                  <Tr>
+                    <Th>Company</Th>
+                    <Th>Premium</Th>
+                    <Th>Policy Type</Th>
+                    <Th>IDV</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {CarPolicyBazar?.map((el: any, index: number) => {
+                    return (
+                      <>
+                        {el.provider === "bajaj" ? (
+                          <Tr>
+                            <Td>{el.provider}</Td>
+                            <Td>{el.premiumAmount}</Td>
+                            <Td>{el.policy}</Td>
+                            <Td>{el.provider}</Td>
+                          </Tr>
+                        ) : (
+                          <></>
+                        )}
+                      </>
+                    );
+                  })}
+                </Tbody>
+              </Table>
+            </TableContainer>
+            <TableContainer>
+              <Text
+                ml={2}
+                fontWeight="bold"
+                textTransform="uppercase"
+                fontSize="lg"
+                letterSpacing="wide"
+                color="teal.600"
+              >
+                HDFC Ergo
+              </Text>
+              <Table variant="simple">
+                <Thead>
+                  <Tr>
+                    <Th>Company</Th>
+                    <Th>Premium</Th>
+                    <Th>Policy Type</Th>
+                    <Th>IDV</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {CarHdfcErgo?.map((el: any, index: number) => {
+                    return (
+                      <>
+                        {el.provider === "bajaj" ? (
+                          <Tr>
+                            <Td>{el.provider}</Td>
+                            <Td>{el.premiumAmount}</Td>
+                            <Td>{el.policy}</Td>
+                            <Td>{el.provider}</Td>
+                          </Tr>
+                        ) : (
+                          <></>
+                        )}
+                      </>
+                    );
+                  })}
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </TabPanel>
+          <TabPanel>
+            <TableContainer>
+              <Text
+                ml={2}
+                fontWeight="bold"
+                textTransform="uppercase"
+                fontSize="lg"
+                letterSpacing="wide"
+                color="teal.600"
+              >
+                Insurance Dekho
+              </Text>
+              <Table variant="simple">
+                <Thead>
+                  <Tr>
+                    <Th>Company</Th>
+                    <Th>Premium</Th>
+                    <Th>Policy Type</Th>
+                    <Th>IDV</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {CarInsuranceDekho?.map((el: any, index: number) => {
+                    return (
+                      <>
+                        {el.provider === "digit" ? (
+                          <Tr>
+                            <Td>{el.provider}</Td>
+                            <Td>{el.premiumAmount}</Td>
+                            <Td>{el.policy}</Td>
+                            <Td>{el.idv}</Td>
+                          </Tr>
+                        ) : (
+                          <></>
+                        )}
+                      </>
+                    );
+                  })}
+                </Tbody>
+              </Table>
+            </TableContainer>
+            <TableContainer>
+              <Text
+                ml={2}
+                fontWeight="bold"
+                textTransform="uppercase"
+                fontSize="lg"
+                letterSpacing="wide"
+                color="teal.600"
+              >
+                Policy Bazar
+              </Text>
+              <Table variant="simple">
+                <Thead>
+                  <Tr>
+                    <Th>Company</Th>
+                    <Th>Premium</Th>
+                    <Th>Policy Type</Th>
+                    <Th>IDV</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {CarPolicyBazar?.map((el: any, index: number) => {
+                    return (
+                      <>
+                        {el.provider === "digit" ? (
+                          <Tr>
+                            <Td>{el.provider}</Td>
+                            <Td>{el.premiumAmount}</Td>
+                            <Td>{el.policy}</Td>
+                            <Td>{el.provider}</Td>
+                          </Tr>
+                        ) : (
+                          <></>
+                        )}
+                      </>
+                    );
+                  })}
+                </Tbody>
+              </Table>
+            </TableContainer>
+            <TableContainer>
+              <Text
+                ml={2}
+                fontWeight="bold"
+                textTransform="uppercase"
+                fontSize="lg"
+                letterSpacing="wide"
+                color="teal.600"
+              >
+                HDFC Ergo
+              </Text>
+              <Table variant="simple">
+                <Thead>
+                  <Tr>
+                    <Th>Company</Th>
+                    <Th>Premium</Th>
+                    <Th>Policy Type</Th>
+                    <Th>IDV</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {CarHdfcErgo?.map((el: any, index: number) => {
+                    return (
+                      <>
+                        {el.provider === "digit" ? (
+                          <Tr>
+                            <Td>{el.provider}</Td>
+                            <Td>{el.premiumAmount}</Td>
+                            <Td>{el.policy}</Td>
+                            <Td>{el.provider}</Td>
+                          </Tr>
+                        ) : (
+                          <></>
+                        )}
+                      </>
+                    );
+                  })}
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </TabPanel>
+          <TabPanel>
+            <TableContainer>
+              <Text
+                ml={2}
+                fontWeight="bold"
+                textTransform="uppercase"
+                fontSize="lg"
+                letterSpacing="wide"
+                color="teal.600"
+              >
+                Insurance Dekho
+              </Text>
+              <Table variant="simple">
+                <Thead>
+                  <Tr>
+                    <Th>Company</Th>
+                    <Th>Premium</Th>
+                    <Th>Policy Type</Th>
+                    <Th>IDV</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {CarInsuranceDekho?.map((el: any, index: number) => {
+                    return (
+                      <>
+                        {el.provider === "Sriram" ? (
+                          <Tr>
+                            <Td>{el.provider}</Td>
+                            <Td>{el.premiumAmount}</Td>
+                            <Td>{el.policy}</Td>
+                            <Td>{el.idv}</Td>
+                          </Tr>
+                        ) : (
+                          <></>
+                        )}
+                      </>
+                    );
+                  })}
+                </Tbody>
+              </Table>
+            </TableContainer>
+            <TableContainer>
+              <Text
+                ml={2}
+                fontWeight="bold"
+                textTransform="uppercase"
+                fontSize="lg"
+                letterSpacing="wide"
+                color="teal.600"
+              >
+                Policy Bazar
+              </Text>
+              <Table variant="simple">
+                <Thead>
+                  <Tr>
+                    <Th>Company</Th>
+                    <Th>Premium</Th>
+                    <Th>Policy Type</Th>
+                    <Th>IDV</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {CarPolicyBazar?.map((el: any, index: number) => {
+                    return (
+                      <>
+                        {el.provider === "Sriram" ? (
+                          <Tr>
+                            <Td>{el.provider}</Td>
+                            <Td>{el.premiumAmount}</Td>
+                            <Td>{el.policy}</Td>
+                            <Td>{el.provider}</Td>
+                          </Tr>
+                        ) : (
+                          <></>
+                        )}
+                      </>
+                    );
+                  })}
+                </Tbody>
+              </Table>
+            </TableContainer>
+            <TableContainer>
+              <Text
+                ml={2}
+                fontWeight="bold"
+                textTransform="uppercase"
+                fontSize="lg"
+                letterSpacing="wide"
+                color="teal.600"
+              >
+                HDFC Ergo
+              </Text>
+              <Table variant="simple">
+                <Thead>
+                  <Tr>
+                    <Th>Company</Th>
+                    <Th>Premium</Th>
+                    <Th>Policy Type</Th>
+                    <Th>IDV</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {CarHdfcErgo?.map((el: any, index: number) => {
+                    return (
+                      <>
+                        {el.provider === "Sriram" ? (
+                          <Tr>
+                            <Td>{el.provider}</Td>
+                            <Td>{el.premiumAmount}</Td>
+                            <Td>{el.policy}</Td>
+                            <Td>{el.provider}</Td>
+                          </Tr>
+                        ) : (
+                          <></>
+                        )}
+                      </>
+                    );
+                  })}
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </TabPanel>
+          <TabPanel>
+            <TableContainer>
+              <Text
+                ml={2}
+                fontWeight="bold"
+                textTransform="uppercase"
+                fontSize="lg"
+                letterSpacing="wide"
+                color="teal.600"
+              >
+                Insurance Dekho
+              </Text>
+              <Table variant="simple">
+                <Thead>
+                  <Tr>
+                    <Th>Company</Th>
+                    <Th>Premium</Th>
+                    <Th>Policy Type</Th>
+                    <Th>IDV</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {CarInsuranceDekho?.map((el: any, index: number) => {
+                    return (
+                      <>
+                        {el.provider === "United India Insurance" ? (
+                          <Tr>
+                            <Td>{el.provider}</Td>
+                            <Td>{el.premiumAmount}</Td>
+                            <Td>{el.policy}</Td>
+                            <Td>{el.idv}</Td>
+                          </Tr>
+                        ) : (
+                          <></>
+                        )}
+                      </>
+                    );
+                  })}
+                </Tbody>
+              </Table>
+            </TableContainer>
+            <TableContainer>
+              <Text
+                ml={2}
+                fontWeight="bold"
+                textTransform="uppercase"
+                fontSize="lg"
+                letterSpacing="wide"
+                color="teal.600"
+              >
+                Policy Bazar
+              </Text>
+              <Table variant="simple">
+                <Thead>
+                  <Tr>
+                    <Th>Company</Th>
+                    <Th>Premium</Th>
+                    <Th>Policy Type</Th>
+                    <Th>IDV</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {CarPolicyBazar?.map((el: any, index: number) => {
+                    return (
+                      <>
+                        {el.provider === "United India Insurance" ? (
+                          <Tr>
+                            <Td>{el.provider}</Td>
+                            <Td>{el.premiumAmount}</Td>
+                            <Td>{el.policy}</Td>
+                            <Td>{el.provider}</Td>
+                          </Tr>
+                        ) : (
+                          <></>
+                        )}
+                      </>
+                    );
+                  })}
+                </Tbody>
+              </Table>
+            </TableContainer>
+            <TableContainer>
+              <Text
+                ml={2}
+                fontWeight="bold"
+                textTransform="uppercase"
+                fontSize="lg"
+                letterSpacing="wide"
+                color="teal.600"
+              >
+                HDFC Ergo
+              </Text>
+              <Table variant="simple">
+                <Thead>
+                  <Tr>
+                    <Th>Company</Th>
+                    <Th>Premium</Th>
+                    <Th>Policy Type</Th>
+                    <Th>IDV</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {CarHdfcErgo?.map((el: any, index: number) => {
+                    return (
+                      <>
+                        {el.provider === "United India Insurance" ? (
+                          <Tr>
+                            <Td>{el.provider}</Td>
+                            <Td>{el.premiumAmount}</Td>
+                            <Td>{el.policy}</Td>
+                            <Td>{el.provider}</Td>
+                          </Tr>
+                        ) : (
+                          <></>
+                        )}
+                      </>
+                    );
+                  })}
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </div>
   );
 };
